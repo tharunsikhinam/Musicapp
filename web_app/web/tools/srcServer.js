5cd0b9f54acfd5bdd43e08bf0f87bc30a5bbc1e9
Quikr <quikr@CFADMINs-MacBook-Pro.local>
2016-08-30 01:32:40 +0530

    socket

--- web_app/web/tools/srcServer.js
+++ web_app/web/tools/srcServer.js
@@ -1,16 +1,19 @@
 /**
  * Created by quikr on 7/12/16.
  */
-import express from "express";
-import webpack from "webpack";
+ import webpack from "webpack";
 import path from "path";
 import config from "../webpack.config.dev";
 import open from "open";
+import express from 'express';
+var app = require('express')();
+var http = require('http').Server(app);
+
 
 /* eslint-disable no-console */
 
 const port = 2323;
-const app = express();
+ var io = require('socket.io')(http);
 const compiler = webpack(config);
 console.log(path.resolve(__dirname + '/../public'));
 
@@ -31,9 +34,21 @@ app.get('*', function(req, res) {
 //console.log("reaching here don't");
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+    //io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
 
 
-app.listen(port, function(err) {
+http.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
